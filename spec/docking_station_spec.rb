1aebbe9f46fcb092bcd420185f73a62d9ecb23e7
dearshrewdwit <dearshrewdwit@gmail.com>
2015-10-13 11:51:14 +0100

    Added rspec test for release bike, and method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,12 @@
 require 'docking_station.rb'
 
-describe DockingStation do
+ describe DockingStation do
+   it { is_expected.to respond_to(:release_bike) }
+ end
 
-end
+
+# describe DockingStation do
+#  it 'returns true when DockingStation responds to method' do
+#    expect(DockingStation.new).to respond_to(:release_bike)
+#  end
+# end
